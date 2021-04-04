36d606c6dbf0a950159367bca0d2c33acd22f954
probalazs <probalazs@nyenyere.eu>
2021-04-04 10:54:38 +0200

    feat(app): export pipes
    resolve WMA-467

--- index.ts
+++ index.ts
@@ -1,28 +1,34 @@
-export * from './lib/service';
-export * from './lib/pipe';
+export * from './lib';
 import { NgModule, ModuleWithProviders, Provider } from '@angular/core';
-import { TranslatePipe } from './lib/pipe';
-import { TranslatePartPipe } from './lib/translate-part.pipe';
-import { TranslateChunkPipe } from './lib/translate-chunk.pipe';
-import { TranslateService } from './lib/service';
+import {
+  TranslatePipe,
+  TranslatePartPipe,
+  TranslateChunkPipe,
+  TranslateService,
+} from './lib';
 
 export type Translations = {
-  [name: string]: string
+  [name: string]: string;
 };
 
-export function translationsFactory() { return {}; }
+export function translationsFactory() {
+  return {};
+}
 
 @NgModule({
   declarations: [TranslatePipe, TranslatePartPipe, TranslateChunkPipe],
-  exports: [TranslatePipe, TranslatePartPipe, TranslateChunkPipe]
+  exports: [TranslatePipe, TranslatePartPipe, TranslateChunkPipe],
 })
 export class TranslateModule {
   static forRoot(
-    providedTranslations: Provider = { provide: 'translations', useFactory: translationsFactory }
+    providedTranslations: Provider = {
+      provide: 'translations',
+      useFactory: translationsFactory,
+    }
   ): ModuleWithProviders<TranslateModule> {
     return {
       ngModule: TranslateModule,
-      providers: [TranslateService, providedTranslations]
+      providers: [TranslateService, providedTranslations],
     };
   }
 }
---
+++ lib/index.ts
@@ -0,0 +1,4 @@
+export * from './pipe';
+export * from './service';
+export * from './translate-chunk.pipe';
+export * from './translate-part.pipe';
